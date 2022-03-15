f9d5e8f8ce9302c7bd50dc5c5d52963a0ba639f9
Robert <gross@anatomage.com>
2022-03-15 14:35:25 -0700

    add missing source files

---
+++ Source/UnrealBaseProject/UnrealBaseProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBaseProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBASEPROJECT_API AUnrealBaseProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
